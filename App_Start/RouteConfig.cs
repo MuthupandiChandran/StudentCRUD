7317b344c5ecb7d519d25eb210d483c78077a5c4
Muthupandi Chandran <pp@DESKTOP-S1M0R1Q>
2024-08-20 01:14:58 +0530

    Routing changes-Initial

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace StudentCRUD
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Student", action = "Index", id = UrlParameter.Optional }
+            name: "Default",
+            url: "{*url}",
+            defaults: new { controller = "Home", action = "Index" }
             );
         }
     }
