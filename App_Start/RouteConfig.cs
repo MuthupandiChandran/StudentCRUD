1bd6af9e3cac78759829b3f75cb5c3df643575b2
Muthupandi Chandran <pp@DESKTOP-S1M0R1Q>
2024-08-22 22:44:29 +0530

    Route-Config file Changes

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace StudentCRUD
 
             routes.MapRoute(
             name: "Default",
-            url: "{*url}",
-            defaults: new { controller = "Home", action = "Index" }
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Student", action = "Index",id= UrlParameter.Optional }
             );
         }
     }
